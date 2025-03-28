789577d2b818c378ba1cd1c240f2d94ce9e597d3
switch <3440501841@qq.com>
2025-03-28 12:37:34 +0800

    children无法正确渲染

--- src/components/sandbox/SideMenu.jsx
+++ src/components/sandbox/SideMenu.jsx
@@ -11,10 +11,12 @@ import {
   FormOutlined,
   HomeOutlined,
 } from '@ant-design/icons'
-
+import './index.css'
+import { useLocation } from 'react-router-dom'
+ 
 const { SubMenu } = Menu
 const { Sider } = Layout
-
+ 
 // **手动映射菜单项对应的图标**
 const iconMap = {
   首页: <HomeOutlined />,
@@ -25,26 +27,27 @@ const iconMap = {
   审核管理: <AuditOutlined />,
   发布管理: <UploadOutlined />,
 }
-
+ 
 function SideMenu() {
   const [menu, setMenu] = useState([])
+  const location = useLocation() // 获取当前的路径
   useEffect(() => {
     axios.get('http://localhost:3000/rights?_embed=children').then((res) => {
       setMenu(res.data)
     })
   }, [])
-
+ 
   const navigate = useNavigate()
-
+ 
   const checkPermission = (item) => {
     // 检查用户是否具有访问权限
     return item.pagepermisson === 1
   }
-
+ 
   const renderMenu = (menuList) => {
     return menuList.map((item) => {
       const icon = iconMap[item.title] || <VideoCameraOutlined /> // 默认图标
-      if (item.children && checkPermission(item)) {
+      if (item.children?.length > 0 && checkPermission(item)) {
         return (
           <SubMenu key={item.key} icon={icon} title={item.title}>
             {renderMenu(item.children)}
@@ -52,26 +55,43 @@ function SideMenu() {
         )
       }
       return (
-        checkPermission(item) &&
-        <Menu.Item key={item.key} icon={icon} onClick={() => navigate(item.key)}>
-          {item.title}
-        </Menu.Item>
+        checkPermission(item) && (
+          <Menu.Item
+            key={item.key}
+            icon={icon}
+            onClick={() => navigate(item.key)}
+          >
+            {item.title}
+          </Menu.Item>
+        )
       )
     })
   }
-
+ 
+  //找到路径
+  const selectKeys = [location.pathname]
+  //分割字符串
+  const openKeys = ['/' + location.pathname.split('/')[1]]
   return (
     <Sider trigger={null} collapsible>
-      <div className="logo">新闻发布系统</div>
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-        {renderMenu(menu)}
-      </Menu>
+      <div style={{ display: 'flex', height: '100%', flexDirection: 'column' }}>
+        <div className="logo">新闻发布系统</div>
+        <div style={{ flex: 1, overflow: 'auto' }}>
+          <Menu
+            theme="dark"
+            mode="inline"
+            selectedKeys={selectKeys}
+            defaultOpenKeys={openKeys}
+          >
+            {renderMenu(menu)}
+          </Menu>
+        </div>
+      </div>
     </Sider>
   )
 }
-
+ 
 export default SideMenu
-
 
 
 // import React, { useState, useEffect } from 'react'
@@ -87,10 +107,10 @@ export default SideMenu
 //   FormOutlined,
 //   HomeOutlined,
 // } from '@ant-design/icons'
-
+ 
 // const { SubMenu } = Menu
 // const { Sider } = Layout
-
+ 
 // // **手动映射菜单项对应的图标**
 // const iconMap = {
 //   首页: <HomeOutlined />,
@@ -101,21 +121,35 @@ export default SideMenu
 //   审核管理: <AuditOutlined />,
 //   发布管理: <UploadOutlined />,
 // }
-
+ 
 // function SideMenu() {
+//   useEffect(() => {
+//     axios.get('http://localhost:3000/rights?_embed=children')
+//       .then(res => {
+//         console.log('获取到的菜单数据:', res.data) // 🔍 调试菜单数据
+//         setMenu(res.data)
+//       })
+//       .catch(error => console.error('获取菜单数据失败:', error))
+//   }, [])
+  
 //   const [menu, setMenu] = useState([])
 //   useEffect(() => {
 //     axios.get('http://localhost:3000/rights?_embed=children').then((res) => {
 //       setMenu(res.data)
 //     })
 //   }, [])
-
+ 
 //   const navigate = useNavigate()
-
+ 
+//   const checkPermission = (item) => {
+//     // 检查用户是否具有访问权限
+//     return item.pagepermisson === 1
+//   }
+ 
 //   const renderMenu = (menuList) => {
 //     return menuList.map((item) => {
 //       const icon = iconMap[item.title] || <VideoCameraOutlined /> // 默认图标
-//       if (item.children?.length > 0) {
+//       if (item.children && checkPermission(item)) {
 //         return (
 //           <SubMenu key={item.key} icon={icon} title={item.title}>
 //             {renderMenu(item.children)}
@@ -123,13 +157,14 @@ export default SideMenu
 //         )
 //       }
 //       return (
+//         checkPermission(item) &&
 //         <Menu.Item key={item.key} icon={icon} onClick={() => navigate(item.key)}>
 //           {item.title}
 //         </Menu.Item>
 //       )
 //     })
 //   }
-
+ 
 //   return (
 //     <Sider trigger={null} collapsible>
 //       <div className="logo">新闻发布系统</div>
@@ -139,9 +174,8 @@ export default SideMenu
 //     </Sider>
 //   )
 // }
-
+ 
 // export default SideMenu
-
 
 
 // // import React, { useState, useEffect } from 'react'
@@ -153,20 +187,30 @@ export default SideMenu
 // //   SettingOutlined,
 // //   UploadOutlined,
 // //   VideoCameraOutlined,
+// //   AuditOutlined,
+// //   FormOutlined,
+// //   HomeOutlined,
 // // } from '@ant-design/icons'
+// // import './index.css'
+// // import { useLocation } from 'react-router-dom'
 
 // // const { SubMenu } = Menu
 // // const { Sider } = Layout
 
+// // // **手动映射菜单项对应的图标**
 // // const iconMap = {
-// //   user: <UserOutlined />,
-// //   setting: <SettingOutlined />,
-// //   upload: <UploadOutlined />,
-// //   video: <VideoCameraOutlined />,
+// //   首页: <HomeOutlined />,
+// //   用户管理: <UserOutlined />,
+// //   用户列表: <UserOutlined />,
+// //   权限管理: <SettingOutlined />,
+// //   新闻管理: <FormOutlined />,
+// //   审核管理: <AuditOutlined />,
+// //   发布管理: <UploadOutlined />,
 // // }
 
 // // function SideMenu() {
 // //   const [menu, setMenu] = useState([])
+// //   const location = useLocation() // 获取当前的路径
 // //   useEffect(() => {
 // //     axios.get('http://localhost:3000/rights?_embed=children').then((res) => {
 // //       setMenu(res.data)
@@ -175,10 +219,15 @@ export default SideMenu
 
 // //   const navigate = useNavigate()
 
+// //   const checkPermission = (item) => {
+// //     // 检查用户是否具有访问权限
+// //     return item.pagepermisson === 1
+// //   }
+
 // //   const renderMenu = (menuList) => {
 // //     return menuList.map((item) => {
-// //       const icon = iconMap[item.icon] || null // 映射图标
-// //       if (item.children?.length > 0) {
+// //       const icon = iconMap[item.title] || <VideoCameraOutlined /> // 默认图标
+// //       if (item.children?.length > 0 && checkPermission(item)) {
 // //         return (
 // //           <SubMenu key={item.key} icon={icon} title={item.title}>
 // //             {renderMenu(item.children)}
@@ -186,76 +235,100 @@ export default SideMenu
 // //         )
 // //       }
 // //       return (
-// //         <Menu.Item key={item.key} icon={icon} onClick={() => navigate(item.key)}>
-// //           {item.title}
-// //         </Menu.Item>
+// //         checkPermission(item) && (
+// //           <Menu.Item
+// //             key={item.key}
+// //             icon={icon}
+// //             onClick={() => navigate(item.key)}
+// //           >
+// //             {item.title}
+// //           </Menu.Item>
+// //         )
 // //       )
 // //     })
 // //   }
 
+// //   //找到路径
+// //   const selectKeys = [location.pathname]
+// //   //分割字符串
+// //   const openKeys = ['/' + location.pathname.split('/')[1]]
 // //   return (
 // //     <Sider trigger={null} collapsible>
-// //       <div className="logo">新闻发布系统</div>
-// //       <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-// //         {renderMenu(menu)}
-// //       </Menu>
+// //       <div style={{ display: 'flex', height: '100%', flexDirection: 'column' }}>
+// //         <div className="logo">新闻发布系统</div>
+// //         <div style={{ flex: 1, overflow: 'auto' }}>
+// //           <Menu
+// //             theme="dark"
+// //             mode="inline"
+// //             selectedKeys={selectKeys}
+// //             defaultOpenKeys={openKeys}
+// //           >
+// //             {renderMenu(menu)}
+// //           </Menu>
+// //         </div>
+// //       </div>
 // //     </Sider>
 // //   )
 // // }
 
 // // export default SideMenu
 
-
-
 // // // import React, { useState, useEffect } from 'react'
-// // // import { Layout, Menu, theme } from 'antd'
-// // // import './index.css'
-// // // import {
-// // //   UploadOutlined,
-// // //   UserOutlined,
-// // //   VideoCameraOutlined,
-// // //   SettingOutlined,
-// // // } from '@ant-design/icons'
-// // // import { Switch } from 'antd'
-// // // // import SubMenu from 'antd/es/menu/SubMenu'
-// // // import { useNavigate } from 'react-router-dom' // 引入 useNavigate
+// // // import { Layout, Menu } from 'antd'
+// // // import { useNavigate } from 'react-router-dom'
 // // // import axios from 'axios'
-// // // const { SubMenu } = Menu;  
+// // // import {
+// // //   UserOutlined,
+// // //   SettingOutlined,
+// // //   UploadOutlined,
+// // //   VideoCameraOutlined,
+// // //   AuditOutlined,
+// // //   FormOutlined,
+// // //   HomeOutlined,
+// // // } from '@ant-design/icons'
 
-// // // const { Header, Content, Sider } = Layout
+// // // const { SubMenu } = Menu
+// // // const { Sider } = Layout
 
-// // // function SideMenu(props) {
+// // // // **手动映射菜单项对应的图标**
+// // // const iconMap = {
+// // //   首页: <HomeOutlined />,
+// // //   用户管理: <UserOutlined />,
+// // //   用户列表: <UserOutlined />,
+// // //   权限管理: <SettingOutlined />,
+// // //   新闻管理: <FormOutlined />,
+// // //   审核管理: <AuditOutlined />,
+// // //   发布管理: <UploadOutlined />,
+// // // }
+
+// // // function SideMenu() {
 // // //   const [menu, setMenu] = useState([])
 // // //   useEffect(() => {
 // // //     axios.get('http://localhost:3000/rights?_embed=children').then((res) => {
-// // //       console.log(res.data)
 // // //       setMenu(res.data)
 // // //     })
-// // //   })
-// // //   const [collapsed, setCollapsed] = useState(false)
-// // //   const navigate = useNavigate() // 使用 useNavigate 获取 navigate 函数
+// // //   }, [])
 
-// // //   const onClick = (e) => {
-// // //     console.log('click ', e)
+// // //   const navigate = useNavigate()
+
+// // //   const checkPermission = (item) => {
+// // //     // 检查用户是否具有访问权限
+// // //     return item.pagepermisson === 1
 // // //   }
 
 // // //   const renderMenu = (menuList) => {
 // // //     return menuList.map((item) => {
-// // //       if (item.children) {
+// // //       const icon = iconMap[item.title] || <VideoCameraOutlined /> // 默认图标
+// // //       if (item.children?.length>0 && checkPermission(item)) {
 // // //         return (
-// // //           <SubMenu key={item.key} icon={item.icon} title={item.title}>
+// // //           <SubMenu key={item.key} icon={icon} title={item.title}>
 // // //             {renderMenu(item.children)}
 // // //           </SubMenu>
 // // //         )
 // // //       }
 // // //       return (
-// // //         <Menu.Item
-// // //           key={item.key}
-// // //           icon={item.icon}
-// // //           onClick={() => {
-// // //             navigate(item.key) // 使用 navigate 进行导航
-// // //           }}
-// // //         >
+// // //         checkPermission(item) &&
+// // //         <Menu.Item key={item.key} icon={icon} onClick={() => navigate(item.key)}>
 // // //           {item.title}
 // // //         </Menu.Item>
 // // //       )
@@ -263,16 +336,9 @@ export default SideMenu
 // // //   }
 
 // // //   return (
-// // //     <Sider trigger={null} collapsible collapsed={false}>
+// // //     <Sider trigger={null} collapsible>
 // // //       <div className="logo">新闻发布系统</div>
-// // //       <div className="demo-logo-vertical" />
-// // //       <Menu
-// // //         theme="dark"
-// // //         mode="inline"
-// // //         defaultSelectedKeys={['1']}
-// // //         onClick={onClick}
-// // //         defaultOpenKeys={['sub1']}
-// // //       >
+// // //       <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
 // // //         {renderMenu(menu)}
 // // //       </Menu>
 // // //     </Sider>
