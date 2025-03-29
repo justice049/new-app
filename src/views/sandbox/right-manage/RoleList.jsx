57b4fd2eda19a607d23166bf4008c8960837491b
switch <3440501841@qq.com>
2025-03-29 16:50:28 +0800

    feat(权限管理): 重构角色权限数据结构并实现权限分配功能
    将角色权限从数组结构改为包含checked和halfChecked的对象结构，以便支持更灵活的权限管理。在RoleList组件中新增权限分配功能，使用Tree组件展示权限列表并支持权限的选中与更新。

--- src/views/sandbox/right-manage/RoleList.jsx
+++ src/views/sandbox/right-manage/RoleList.jsx
@@ -1,99 +1,147 @@
-import React, { useState, useEffect } from "react";
-import axios from 'axios';
-import { Button, Table, Modal } from 'antd';
-import { BarsOutlined, DeleteOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Button, Table, Modal,Tree } from 'antd'
+import {
+  BarsOutlined,
+  DeleteOutlined,
+  ExclamationCircleOutlined,
+} from '@ant-design/icons'
 
-const { confirm } = Modal;
+const { confirm } = Modal
 
 function RoleList() {
-    const [dataSource, setdataSource] = useState([]);
-    const [isModalVisible, setIsModalVisible] = useState(false);
+  const [dataSource, setdataSource] = useState([])
+  const [isModalVisible, setIsModalVisible] = useState(false)
+  const [rightList, setRightList] = useState([])
+  //默认选中
+  const [currentRights, setCurrentRights] = useState([])
+  const [currentId,setCurrentId] = useState(0)
 
-    useEffect(() => {
-        axios.get('http://localhost:3000/roles').then(res => {
-            setdataSource(res.data);
-        });
-    }, []);
+  useEffect(() => {
+    axios.get('http://localhost:3000/roles').then((res) => {
+      setdataSource(res.data)
+    })
+  }, [])
 
-    const confirmMethod = (record) => {
-        confirm({
-            title: '确定要删除这个角色吗？',
-            icon: <ExclamationCircleOutlined />,
-            onOk() {
-                deleteMethod(record);
-            },
-            onCancel() {
-                console.log('取消删除');
-            },
-        });
-    };
+  useEffect(() => {
+    axios.get('http://localhost:3000/rights?_embed=children').then((res) => {
+      setRightList(res.data)
+    })
+  }, [])
 
-    const deleteMethod = (record) => {
-        setdataSource(dataSource.filter(item => item.id !== record.id));
-        axios.delete(`http://localhost:3000/roles/${record.id}`);
-    };
+  const confirmMethod = (record) => {
+    confirm({
+      title: '确定要删除这个角色吗？',
+      icon: <ExclamationCircleOutlined />,
+      onOk() {
+        deleteMethod(record)
+      },
+      onCancel() {
+        console.log('取消删除')
+      },
+    })
+  }
 
-    const showModal = () => {
-        setIsModalVisible(true);
-    };
+  const deleteMethod = (record) => {
+    setdataSource(dataSource.filter((item) => item.id !== record.id))
+    axios.delete(`http://localhost:3000/roles/${record.id}`)
+  }
 
-    const handleOk = () => {
-        console.log("点击了确认");
-        setIsModalVisible(false);
-    };
+  const showModal = () => {
+    setIsModalVisible(true)
+    //dataSource重新映射
+  }
 
-    const handleCancel = () => {
-        console.log("点击了取消");
-        setIsModalVisible(false);
-    };
-
-    const columns = [
-        {
-            title: 'ID',
-            dataIndex: 'id',
-            render: (id) => <b>{id}</b>,
-        },
-        {
-            title: '角色名称',
-            dataIndex: 'roleName',
-        },
-        {
-            title: "操作",
-            render: (item) => (
-                <div>
-                    <Button type="primary" shape="circle" icon={<BarsOutlined />} onClick={showModal} />
-                    <Button 
-                        danger 
-                        type="primary" 
-                        shape="circle" 
-                        icon={<DeleteOutlined />} 
-                        onClick={() => confirmMethod(item)}
-                    />
-                </div>
-            ),
+  const handleOk = () => {
+    console.log('点击了确认')
+    setIsModalVisible(false)
+    setdataSource(dataSource.map(item=>{
+        if(item.id === currentId){
+            return{
+                ...item,
+                rights:currentRights
+            }
         }
-    ];
+        else{
+            return item
+        }
+    }))
+    //patch
+    axios.patch(`http://localhost:3000/roles/${currentId}`,{
+        rights:currentRights
+    })
+  }
 
-    return (
+  const handleCancel = () => {
+    console.log('点击了取消')
+    setIsModalVisible(false)
+  }
+
+  // 选中的权限
+  const onCheck = (checkedKeys, checkedInfo) => {
+    setCurrentRights(checkedKeys)
+  }
+
+  const columns = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      render: (id) => <b>{id}</b>,
+    },
+    {
+      title: '角色名称',
+      dataIndex: 'roleName',
+    },
+    {
+      title: '操作',
+      render: (item) => (
         <div>
-            <Table 
-                dataSource={dataSource} 
-                columns={columns} 
-                rowKey={(item) => item.id} 
-            />
-            {/* Modal 组件 */}
-            <Modal 
-                title="权限分配" 
-                open={isModalVisible} 
-                onOk={handleOk} 
-                onCancel={handleCancel}
-            >
-                <p>这里可以放权限分配的内容</p>
-                <p>比如角色权限列表</p>
-                <p>或者其他选项</p>
-            </Modal>
+          <Button
+            type="primary"
+            shape="circle"
+            icon={<BarsOutlined />}
+            onClick={()=>{
+                setIsModalVisible(true)
+                setCurrentRights(item.rights)
+                setCurrentId(item.id)
+            }}
+          />
+          <Button
+            danger
+            type="primary"
+            shape="circle"
+            icon={<DeleteOutlined />}
+            onClick={() => confirmMethod(item)}
+          />
         </div>
-    );
+      ),
+    },
+  ]
+
+  return (
+    <div>
+      <Table
+        dataSource={dataSource}
+        columns={columns}
+        rowKey={(item) => item.id}
+      />
+      {/* Modal 组件 */}
+      <Modal
+        title="权限分配"
+        open={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <Tree
+          checkable 
+          treeData={rightList}
+          checkedKeys={currentRights}
+          checkStrictly = {true} 
+          onCheck={onCheck}
+          />
+      </Modal>
+    </div>
+  )
 }
 
-export default RoleList;
+export default RoleList
