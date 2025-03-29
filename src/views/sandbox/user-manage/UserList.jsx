bf1ae3546eda18c7794fce4bdfa806e1f1d69b65
switch <3440501841@qq.com>
2025-03-29 18:54:25 +0800

    feat(用户管理): 实现用户列表页面并添加表格展示功能
    新增了用户列表页面，使用Ant Design的Table组件展示用户数据，包括区域、角色名称、用户名、用户状态和操作列。通过axios从后端获取用户数据，并添加了分页功能。

--- src/views/sandbox/user-manage/UserList.jsx
+++ src/views/sandbox/user-manage/UserList.jsx
@@ -1,9 +1,75 @@
+import React,{useState,useEffect} from 'react';
+import { Button,Table, Tag,Modal,Popover, Switch } from 'antd';
+import { EditOutlined,DeleteOutlined,ExclamationCircleOutlined } from '@ant-design/icons';
+import axios from 'axios';
+import { data } from 'react-router-dom';
+import Item from 'antd/es/list/Item';
+const { confirm } = Modal;
+
 function UserList() {
-    return (
+    const [dataSource,setdataSource]=useState([])
+    useEffect(()=>{
+        axios.get("http://localhost:3000/users?_expand=role").then(res=>{
+          console.log(res.data);
+          const list = res.data
+          setdataSource(list)
+       })
+    },[])
+    const columns = [
+        {
+          title: '区域',
+          dataIndex: 'region',
+          render:(region)=>{
+            return <b>{region===""?'全球':region}</b>
+        }
+        },
+        {
+          title: '角色名称',
+          dataIndex: 'role',
+          render:(role)=>{
+              return role?.roleName
+          }
+        },
+        {
+          title: '用户名',
+          dataIndex: 'username',
+        },
+        {
+            title:"用户状态",
+            dataIndex:"roleState",
+            render:(roleState,item)=>{
+                return <Switch checked={roleState} disabled={item.default}>
+
+                </Switch>
+            }
+        },
+        {
+          title: '操作',
+          render:(record)=>{
+            return <div>
+               <Button type="primary" shape="circle" icon={<EditOutlined />} disabled={record.default} />
+               <Button danger type="primary" shape="circle" icon={<DeleteOutlined />} disabled={record.default}/>
+            </div>
+          }
+        },
+      ];
+
+      const deleteMethod = (record) => {
+        console.log(record);
+      }
+
+      return (
         <div>
-            UserList page
+            <Button></Button>
+            <Table dataSource={dataSource} columns={columns}
+            pagination={{
+              pageSize:5 
+            }}
+            >
+            rowKey = {item => item.id}
+            </Table>
         </div>
-    );
+      )
 }
 
 export default UserList;
