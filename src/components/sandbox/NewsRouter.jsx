35e85fad82b311c39b3a5040f0f6cfbbc01fe30e
switch <3440501841@qq.com>
2025-04-03 20:00:39 +0800

    feat(路由): 新增新闻管理、审核管理和发布管理的路由组件
    在NewsRouter.jsx中添加了新闻管理、审核管理和发布管理的路由映射，并创建了对应的组件文件。这些组件包括NewsAdd、NewsDraft、NewsCategory、Audit、AuditList、Published、Unpublished和Sunset，用于支持相关功能页面的渲染。

--- src/components/sandbox/NewsRouter.jsx
+++ src/components/sandbox/NewsRouter.jsx
@@ -9,6 +9,31 @@ import { Navigate } from "react-router-dom";
 import Nopermission from "../../views/sandbox/nopermission/Nopermission";
 //引入antd
 import { theme, Layout, ConfigProvider } from "antd";
+import NewsAdd from "../../views/sandbox/news-manage/NewsAdd";
+import NewsDraft from "../../views/sandbox/news-manage/NewsDraft";
+import NewsCategory from "../../views/sandbox/news-manage/NewsCategory";
+import Audit from "../../views/sandbox/audit-manage/Audit";
+import AuditList from "../../views/sandbox/audit-manage/AuditList";
+import Published from "../../views/sandbox/publish-manage/Published";
+import Unpublished from "../../views/sandbox/publish-manage/Unpublished";
+import Sunset from "../../views/sandbox/publish-manage/Sunset";
+
+//创建一个本地的路由映射表
+const LocalRouterMap = {
+    "/home":Home,
+    "user-manage/list":UserList,
+    "right-manage/right/list":RightList,
+    "right-manage/role/list":RoleList,
+    //写什么新闻列表啊，各种权限啊
+    "/news-manage/add":NewsAdd,
+    "/news-manage/draft":NewsDraft,
+    "/news-manage/category":NewsCategory,
+    "/audit-manage/audit":Audit,
+    "/audit-manage/list":AuditList,
+    "publish-manage/published":Published,
+    "/publish-manage/unpublished":Unpublished,
+    "/publish-manage/sunset":Sunset,
+} 
 
 export default function NewsRouter() {
   return (
