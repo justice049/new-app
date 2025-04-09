58ca9b891b0c4b544e8beeda72b3945882b73c0a
switch <3440501841@qq.com>
2025-04-09 20:23:32 +0800

    feat(news-manage): 添加新闻撰写页面的多步骤表单功能
    引入多步骤表单以简化新闻撰写流程，包括基本信息、新闻内容和提交审核三个步骤。使用Ant Design组件库实现UI，并添加导航按钮以在不同步骤间切换。同时，新增News.module.css文件用于样式管理。

--- src/views/sandbox/news-manage/NewsAdd.jsx
+++ src/views/sandbox/news-manage/NewsAdd.jsx
@@ -1,7 +1,144 @@
-import React from 'react'
+import React, { useState,useEffect}  from 'react'
+import { Breadcrumb, Typography, Button, Space,Steps, message,theme,Checkbox, Form, Input, Select } from 'antd';
+import style from './News.module.css'
+
+const { Title } = Typography;
+const description = 'This is a description';
+const onFinish = (values) => {
+  console.log('Success:', values);
+};
+const onFinishFailed = (errorInfo) => {
+  console.log('Failed:', errorInfo);
+};
+const steps = [
+  {
+    title: '基本信息',
+    content: <Form
+    name="basic"
+    labelCol={{
+      span: 4,
+    }}
+    wrapperCol={{
+      span: 20,
+    }}
+    style={{
+      maxWidth: 600,
+      margin: '25px',
+    }}
+    initialValues={{
+      remember: true,
+    }}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete="off"
+  >
+    <Form.Item
+      label="新闻标题"
+      name="title"
+      rules={[
+        {
+          required: true,
+          message: 'Please input your username!',
+        },
+      ]}
+    >
+      <Select>
+          <Option>111</Option>
+          <Option>323</Option>
+          <Option>444</Option>
+        </Select>
+    </Form.Item>
+    <Form.Item
+      label="新闻分类"
+      name="categoryId"
+      rules={[
+        {
+          required: true,
+          message: 'Please input your username!',
+        },
+      ]}
+    >
+      <Select>
+          <Option>111</Option>
+          <Option>323</Option>
+          <Option>444</Option>
+        </Select>
+    </Form.Item>
+  </Form>,
+  },
+  {
+    title: '新闻内容',
+    content: '新闻主体内容',
+  },
+  {
+    title: '新闻提交',
+    content: '保存草稿 审核提交',
+  },
+];
 
 export default function NewsAdd() {
+  const { token } = theme.useToken();
+  const [current, setCurrent] = useState(0);
+  const next = () => {
+    setCurrent(current + 1);
+  };
+  const prev = () => {
+    setCurrent(current - 1);
+  };
+  const items = steps.map((item) => ({
+    key: item.title,
+    title: item.title,
+  }));
   return (
-    <div>NewsAdd</div>
+  <div style={{ marginBottom: 24 }}>
+      <Breadcrumb items={[
+        { title: 'Home' },
+        { title: 'List' },
+        { title: 'App' },
+      ]} />
+      <Space style={{ justifyContent: 'space-between', width: '100%' }}>
+        <Title level={2} style={{ margin: '16px 0' }}>撰写新闻</Title>
+        <Space>
+          <Button>Cancel</Button>
+          <Button type="primary">Submit</Button>
+        </Space>
+      </Space>
+      <Steps current={current} items={items} />
+      <div>{steps[current].content}</div>
+      <div
+        style={{
+          marginTop: 24,
+        }}
+      >
+        {current < steps.length - 1 && (
+          <Button type="primary" onClick={() => next()}>
+            Next
+          </Button>
+        )}
+        
+        {current === steps.length - 1 && (
+        <>
+        <Button type="primary" style={{
+              margin: '0 8px',
+            }} onClick={() => message.success('Processing complete!')}>
+            保存草稿
+          </Button>
+           <Button type="primary">
+           提交审核
+         </Button>
+         </>
+        )}
+        {current > 0 && (
+          <Button
+            style={{
+              margin: '0 8px',
+            }}
+            onClick={() => prev()}
+          >
+            Previous
+          </Button>
+        )}
+      </div>
+    </div>
   )
 }
