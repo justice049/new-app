466c36cf7aa31ec9535bf54417c8a4763bcf9fc8
switch <3440501841@qq.com>
2025-04-09 20:50:53 +0800

    feat(news-manage): 在新闻添加页面增加表单验证和分类选择功能
    在新闻添加页面中，引入表单验证功能，确保用户在进入下一步之前填写必要信息。同时，通过API获取新闻分类列表并动态渲染到选择框中，提升用户体验。

--- src/views/sandbox/news-manage/NewsAdd.jsx
+++ src/views/sandbox/news-manage/NewsAdd.jsx
@@ -1,103 +1,138 @@
-import React, { useState,useEffect}  from 'react'
-import { Breadcrumb, Typography, Button, Space,Steps, message,theme,Checkbox, Form, Input, Select } from 'antd';
+import React, { useState, useEffect,useRef } from 'react'
+import {
+  Breadcrumb,
+  Typography,
+  Button,
+  Space,
+  Steps,
+  message,
+  theme,
+  Checkbox,
+  Form,
+  Input,
+  Select,
+} from 'antd'
 import style from './News.module.css'
+import axios from 'axios'
+import { set } from 'nprogress'
 
-const { Title } = Typography;
-const description = 'This is a description';
+const { Title } = Typography
+const description = 'This is a description'
+const { Option } = Select
+
 const onFinish = (values) => {
-  console.log('Success:', values);
-};
+  console.log('Success:', values)
+}
 const onFinishFailed = (errorInfo) => {
-  console.log('Failed:', errorInfo);
-};
-const steps = [
-  {
-    title: '基本信息',
-    content: <Form
-    name="basic"
-    labelCol={{
-      span: 4,
-    }}
-    wrapperCol={{
-      span: 20,
-    }}
-    style={{
-      maxWidth: 600,
-      margin: '25px',
-    }}
-    initialValues={{
-      remember: true,
-    }}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-  >
-    <Form.Item
-      label="新闻标题"
-      name="title"
-      rules={[
-        {
-          required: true,
-          message: 'Please input your username!',
-        },
-      ]}
-    >
-      <Select>
-          <Option>111</Option>
-          <Option>323</Option>
-          <Option>444</Option>
-        </Select>
-    </Form.Item>
-    <Form.Item
-      label="新闻分类"
-      name="categoryId"
-      rules={[
-        {
-          required: true,
-          message: 'Please input your username!',
-        },
-      ]}
-    >
-      <Select>
-          <Option>111</Option>
-          <Option>323</Option>
-          <Option>444</Option>
-        </Select>
-    </Form.Item>
-  </Form>,
-  },
-  {
-    title: '新闻内容',
-    content: '新闻主体内容',
-  },
-  {
-    title: '新闻提交',
-    content: '保存草稿 审核提交',
-  },
-];
+  console.log('Failed:', errorInfo)
+}
 
 export default function NewsAdd() {
-  const { token } = theme.useToken();
-  const [current, setCurrent] = useState(0);
+  const { token } = theme.useToken()
+  const [current, setCurrent] = useState(0)
+  const [categoryList, setCategoryList] = useState([])
+  const NewsForm = useRef(null)
+  const steps = [
+    {
+      title: '基本信息',
+      content: (
+        <Form
+          name="basic"
+          ref = {NewsForm}
+          labelCol={{
+            span: 4,
+          }}
+          wrapperCol={{
+            span: 20,
+          }}
+          style={{
+            maxWidth: 600,
+            margin: '25px',
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="新闻标题"
+            name="title"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your username!',
+              },
+            ]}
+          >
+            <Input></Input>
+          </Form.Item>
+          <Form.Item
+            label="新闻分类"
+            name="categoryId"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your username!',
+              },
+            ]}
+          >
+            <Select>
+              {categoryList.map((item) => (
+                <Select.Option value={item.id} key={item.id}>
+                  {item.title}
+                </Select.Option>
+              ))}
+            </Select>
+          </Form.Item>
+        </Form>
+      ),
+    },
+    {
+      title: '新闻内容',
+      content: '新闻主体内容',
+    },
+    {
+      title: '新闻提交',
+      content: '保存草稿 审核提交',
+    },
+  ]
   const next = () => {
-    setCurrent(current + 1);
-  };
+    if(current === 0){
+      NewsForm.current.validateFields().then((res) => {
+        console.log(res)
+        setCurrent(current + 1)
+      }).catch((err) => {
+        console.log(err)
+      })
+    }
+    else{
+      setCurrent(current + 1)
+    }
+  }
   const prev = () => {
-    setCurrent(current - 1);
-  };
+    setCurrent(current - 1)
+  }
   const items = steps.map((item) => ({
     key: item.title,
     title: item.title,
-  }));
+  }))
+  useEffect(() => {
+    axios.get('/categories').then((res) => {
+      setCategoryList(res.data)
+    })
+  }, [])
+
   return (
-  <div style={{ marginBottom: 24 }}>
-      <Breadcrumb items={[
-        { title: 'Home' },
-        { title: 'List' },
-        { title: 'App' },
-      ]} />
+    <div style={{ marginBottom: 24 }}>
+      <Breadcrumb
+        items={[{ title: 'Home' }, { title: 'List' }, { title: 'App' }]}
+      />
       <Space style={{ justifyContent: 'space-between', width: '100%' }}>
-        <Title level={2} style={{ margin: '16px 0' }}>撰写新闻</Title>
+        <Title level={2} style={{ margin: '16px 0' }}>
+          撰写新闻
+        </Title>
         <Space>
           <Button>Cancel</Button>
           <Button type="primary">Submit</Button>
@@ -115,18 +150,20 @@ export default function NewsAdd() {
             Next
           </Button>
         )}
-        
+
         {current === steps.length - 1 && (
-        <>
-        <Button type="primary" style={{
-              margin: '0 8px',
-            }} onClick={() => message.success('Processing complete!')}>
-            保存草稿
-          </Button>
-           <Button type="primary">
-           提交审核
-         </Button>
-         </>
+          <>
+            <Button
+              type="primary"
+              style={{
+                margin: '0 8px',
+              }}
+              onClick={() => message.success('Processing complete!')}
+            >
+              保存草稿
+            </Button>
+            <Button type="primary">提交审核</Button>
+          </>
         )}
         {current > 0 && (
           <Button
