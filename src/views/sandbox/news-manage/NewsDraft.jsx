8cfd6bd05fd9558c6e57594cdda418b43de5cd77
switch <3440501841@qq.com>
2025-04-16 19:59:39 +0800

    feat(news-manage): 添加新闻提交审核功能
    在NewsDraft组件中新增handleCheck方法，用于将新闻提交审核并更新auditState状态。提交成功后，用户将被导航至审核列表页面，并收到通知提示。

--- src/views/sandbox/news-manage/NewsDraft.jsx
+++ src/views/sandbox/news-manage/NewsDraft.jsx
@@ -1,95 +1,141 @@
-import React,{useState,useEffect} from 'react';
-import { Button,Table,Modal} from 'antd';
-import { EditOutlined,DeleteOutlined,VerticalAlignTopOutlined,UploadOutlined,ExclamationCircleOutlined } from '@ant-design/icons';
-import axios from 'axios';
-import { data } from 'react-router-dom';
-import { render } from 'nprogress';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
+import { Button, Table, Modal } from 'antd'
+import {
+  EditOutlined,
+  DeleteOutlined,
+  VerticalAlignTopOutlined,
+  UploadOutlined,
+  ExclamationCircleOutlined,
+} from '@ant-design/icons'
+import axios from 'axios'
+import { data } from 'react-router-dom'
+import { render } from 'nprogress'
+import { useNavigate } from 'react-router-dom'
+import { notification } from 'antd'
 
-const { confirm } = Modal;
+const { confirm } = Modal
 
 function NewsDraft(props) {
-    const [dataSource,setdataSource]=useState([])
-    const navigate = useNavigate();
-    const {username} = JSON.parse(localStorage.getItem('token'))
-    useEffect(()=>{
-        axios.get(`/news?author=${username}&auditState=0&_expand=category`).then(res=>{
-          const list = res.data
-          setdataSource(list)
-       })
-    },[username])
-    const columns = [
-        {
-          title: 'ID',
-          dataIndex: 'id',
-          render:(id)=>{
-            return <b>{id}</b>
-          }
-        },
-        {
-          title: '新闻标题',
-          dataIndex: 'title',
-          render:(title,item)=>{
-              return <a href={`#/news-manage/preview/${item.id}`}>{title}</a>
-          }
-        },
-        {
-          title: '作者',
-          dataIndex: 'author',
-        },
-        {
-          title: '分类',
-          dataIndex: 'category',
-          render:(category)=>{
-              return category.title
-          }
-        },
-        {
-          title: '操作',
-          render:(record)=>{
-            return <div>
-               <Button type="primary" shape="circle" icon={<EditOutlined />} onClick={()=>{
-                    navigate(`/news-manage/update/${record.id}`);
-               }} />
-               {/* 如果没有配置权限，就不显示 */}
-               <Button danger type="primary" shape="circle" icon={<DeleteOutlined />} onClick=
-               {()=>confirmMethod(record)}/>
-                 <Button shape="circle" icon={<UploadOutlined />}/>
-            </div>
-          }
-        },
-      ];
-
-      const confirmMethod = (record) => {
-        confirm({
-          title: 'Do you Want to delete these items?',
-          icon: <ExclamationCircleOutlined />,
-          onOk() {
-            deleteMethod(record)
-          },
-          onCancel() {
-            console.log('Cancel');
-          },
-        });
-        console.log('确认删除')
-      };
-
-      const deleteMethod = (record) => {
-        // console.log(record);
-        setdataSource(dataSource.filter(data=>data.id !== record.id))
-        axios.delete(`/news/${record.id}`)
-    };
-
-    return (
-        <div>
-            <Table dataSource={dataSource} columns={columns} pagination={{
-              //一页显示几条数据
-              pageSize:5
-            }}
-            rowKey={record=>record.id}
+  const [dataSource, setdataSource] = useState([])
+  const navigate = useNavigate()
+  const { username } = JSON.parse(localStorage.getItem('token'))
+  useEffect(() => {
+    axios
+      .get(`/news?author=${username}&auditState=0&_expand=category`)
+      .then((res) => {
+        const list = res.data
+        setdataSource(list)
+      })
+  }, [username])
+  const columns = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      render: (id) => {
+        return <b>{id}</b>
+      },
+    },
+    {
+      title: '新闻标题',
+      dataIndex: 'title',
+      render: (title, item) => {
+        return <a href={`#/news-manage/preview/${item.id}`}>{title}</a>
+      },
+    },
+    {
+      title: '作者',
+      dataIndex: 'author',
+    },
+    {
+      title: '分类',
+      dataIndex: 'category',
+      render: (category) => {
+        return category.title
+      },
+    },
+    {
+      title: '操作',
+      render: (record) => {
+        return (
+          <div>
+            <Button
+              type="primary"
+              shape="circle"
+              icon={<EditOutlined />}
+              onClick={() => {
+                navigate(`/news-manage/update/${record.id}`)
+              }}
             />
-        </div>
-    );
+            {/* 如果没有配置权限，就不显示 */}
+            <Button
+              danger
+              type="primary"
+              shape="circle"
+              icon={<DeleteOutlined />}
+              onClick={() => confirmMethod(record)}
+            />
+            {/* 在这添加提交事件 */}
+            <Button
+              shape="circle"
+              icon={<UploadOutlined />}
+              onClick={() => handleCheck(record.id)}
+            />
+          </div>
+        )
+      },
+    },
+  ]
+
+  //做补丁请求
+  const handleCheck = (id) => {
+    axios
+      .patch(`/news/${id}`, {
+        // 正在审核
+        auditState: 1,
+      })
+      .then((res) => {
+        navigate('/audit-manage/list')
+        notification.info({
+          message: `通知`,
+          description: `您可以到审核列表查看您的新闻`,
+          placement: 'bottomRight',
+        })
+      })
+  }
+
+  const confirmMethod = (record) => {
+    confirm({
+      title: 'Do you Want to delete these items?',
+      icon: <ExclamationCircleOutlined />,
+      onOk() {
+        deleteMethod(record)
+      },
+      onCancel() {
+        console.log('Cancel')
+      },
+    })
+    console.log('确认删除')
+  }
+
+  const deleteMethod = (record) => {
+    // console.log(record);
+    setdataSource(dataSource.filter((data) => data.id !== record.id))
+    axios.delete(`/news/${record.id}`)
+  }
+
+  return (
+    <div>
+      <Table
+        dataSource={dataSource}
+        columns={columns}
+        pagination={{
+          //一页显示几条数据
+          pageSize: 5,
+        }}
+        rowKey={(record) => record.id}
+      />
+    </div>
+  )
 }
 
-export default NewsDraft;
+export default NewsDraft
