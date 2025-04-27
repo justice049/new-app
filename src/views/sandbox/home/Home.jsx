bf1fbb72bfb4be592e6a43c037b9b56244d410c4
switch <3440501841@qq.com>
2025-04-27 21:51:28 +0800

    feat: 添加 echarts 图表库并集成到 Home 页面
    在 Home 页面中引入了 echarts 图表库，用于展示销售数据的柱状图。通过 useEffect 初始化图表并配置相关数据，提升页面的数据可视化能力。

--- src/views/sandbox/home/Home.jsx
+++ src/views/sandbox/home/Home.jsx
@@ -4,6 +4,7 @@ import { Card, Col, Row, List } from 'antd'
 import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
 import { Avatar } from 'antd';
 import axios from 'axios';
+import echarts from 'echarts';
 import { useState } from'react'
 const { Meta } = Card;
 
@@ -23,6 +24,34 @@ function Home() {
         }).catch(err => {
          console.error('Request failed', err)
        })
+     },[])
+     useEffect(()=>{
+        var myChart = echarts.init(document.getElementById('main'));
+
+      // 指定图表的配置项和数据
+      var option = {
+        title: {
+          text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        legend: {
+          data: ['销量']
+        },
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [
+          {
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      };
+
+      // 使用刚指定的配置项和数据显示图表。
+      myChart.setOption(option);
      },[])
      const {username,region,role:{roleName}} = JSON.parse(localStorage.getItem('token'))
   return (
@@ -79,6 +108,12 @@ function Home() {
           </Card>
         </Col>
       </Row>
+
+      <div id="main" style={{
+        height:"400px",
+        marginTop:"30px",
+      }}></div>
+
     </div>
   )
 }
