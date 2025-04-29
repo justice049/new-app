4c28aa80eab218490b9e80a5c9d6a0b94c571894
switch <3440501841@qq.com>
2025-04-29 19:52:48 +0800

    feat(router): 添加新闻和新闻详情页面路由
    新增新闻页面（News.jsx）和新闻详情页面（Detail.jsx），并在路由配置中注册相关路径，以支持新闻模块的导航功能。

--- src/router/IndexRouter.jsx
+++ src/router/IndexRouter.jsx
@@ -2,11 +2,21 @@ import { createHashRouter } from 'react-router-dom';
 import Login from '../views/login/Login';
 import NewSandbox from '../views/sandbox/NewSandBox';
 import ProtectedRoute from '../ProtectedRoute';
+import News from '../views/news/News';
+import Detail from '../views/news/Detail';
 
 const router = createHashRouter([
   {
     path: '/login',
     element: <Login />,
+  },
+  {
+    path: '/news',
+    element: <News />,
+  },
+  {
+    path: '/detail/:id',
+    element: <Detail />,
   },
   {
     path: '/*',
